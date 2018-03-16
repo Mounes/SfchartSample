9117f64b765482c73ce6590e0dd094579428730e
Mounes <mounessss@gmail.com>
2018-03-16 16:45:41 +0300

    Initial check-in of module syncFuctionApp_Charts

---
+++ ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+namespace syncFuctionApp_Charts
+{
+    public class ViewModel
+    {
+        
+        public List<DataModel> dataBar1 { get; set; }
+        public List<DataModel> dataBar2 { get; set; }
+
+        public ViewModel()
+        {
+            dataBar1 = new List<DataModel>()
+            {
+                new DataModel("Books",133),
+                new DataModel("Movies",420),
+                new DataModel("Music",500),
+                new DataModel("Games",312)
+            };
+            dataBar2 = new List<DataModel>()
+            {
+                new DataModel("Books",10),
+                new DataModel("Movies",20),
+                new DataModel("Music",70),
+                new DataModel("Games",120)
+            };
+        }
+
+        public class DataModel
+        {
+            public string Category { get; set; }
+            public double Sales { get; set; }
+            public DataModel(string category , double sales)
+            {
+                Category = category;
+                Sales = sales;
+            }
+        }
+    }
+}
---
+++ syncFuctionApp_ChartsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Syncfusion.SfChart;
+using System.Threading.Tasks;
+namespace syncFuctionApp_Charts
+{
+    public partial class syncFuctionApp_ChartsPage : ContentPage
+    {
+        public syncFuctionApp_ChartsPage()
+        {
+            InitializeComponent();
+        }
+   
+    }
+}
+
